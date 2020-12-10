b6a4e2222f17da29aa91a9b737e27fca4766dc8e
ricci22 <nandarizki.nasution@gmail.com>
2020-12-10 11:38:30 +0700

    public resource

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,30 @@
 <!doctype html>
 <!--html lang="{{ str_replace('_', '-', app()->getLocale()) }}"-->
 <html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
 
-@include('themes.frame')
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ config('app.name', 'Laravel') }}</title>
+
+  @include('themes.head')
+</head>
+
+<body class="animsition">
+  <div class="page-wrapper">
+    @include('layouts.navbar_mobile')
+    @include('layouts.sidebar')
+    <div class="page-container">
+      @include('layouts.navbar')
+      <div class="main-content">
+        @yield('content')
+      </div>
+    </div>
+  </div>
+  @include('themes.script')
+</body>
 
 </html>
